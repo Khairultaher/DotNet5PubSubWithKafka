a0e3c78371439eb58abf17ebaacbae3c7a811305
Khairul Taher <khairultaher@outlook.com>
2020-11-04 16:10:45 +0600

    OK

---
+++ Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+
+namespace Publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            var i = 0;
+            while (true)
+            {
+                var message = new Message<Null, string>
+                {
+                    Value = $"Message #{++i}"
+                };
+
+                var dr = await p.ProduceAsync("testTopic", message);
+                Console.WriteLine($"Produced message '{dr.Value}' to topic {dr.Topic}, partition {dr.Partition}, offset {dr.Offset}");
+                Thread.Sleep(5000);
+            }
+            _ = Console.ReadLine();
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "Test",
+                BootstrapServers = "localhost:9092",
+                EnableAutoCommit = false
+            };
+            using var c = new ConsumerBuilder<Ignore, string>(config).Build();
+            c.Subscribe("testTopic");
+
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+            try
+            {
+                while (true)
+                {
+                    var cr = c.Consume(cts.Token);
+                    Console.WriteLine($"Consumed message '{cr.Value}' from topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+    }
+}
